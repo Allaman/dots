34323f1503c0fb7a474e6a13e192c4e14699e13e
Michael <michaeljohannpeter@gmail.com>
2022-10-10 13:20:41 +0200

    change dot_config permission

--- private_dot_config/sketchybar/items/executable_battery.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-sketchybar -m --add item battery right \
-              --set battery update_freq=120 \
-                    updates=$HAS_BATTERY \
-                    battery script="$PLUGIN_DIR/battery.sh" \
-                    battery icon=
