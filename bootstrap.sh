10a76fd05727aa0ceac9c77f59988cd7f6ce4771
Michael <michaeljohannpeter@gmail.com>
2022-10-10 19:59:43 +0200

    remove interactivity from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -65,15 +65,6 @@ main() {
   for program in wget gunzip tar command chmod rm printf mv mkdir; do
     command -v "$program" > /dev/null 2>&1 || { echo "Not found: $program"; exit 1; }
   done
-  log """This will download chezmoi and age and
-run chezmoi init --apply --verbose git@github.com:allaman/dots.git!
-Continue only when you know what you are doing"""
-  select choice in "Continue" "Abort"; do
-    case $choice in
-      Continue ) echo "Going on"; break;;
-      Abort ) exit;;
-    esac
-  done
   get_arch
   get_os
   mkdir -p "${USER_BIN}"
