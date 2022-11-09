3e7bff9b150b2126828534b7b2cc8b4c727220f6
Michael <michaeljohannpeter@gmail.com>
2022-11-09 23:25:53 +0100

    sketchybar: fix misconfiguration

--- dot_config/sketchybar/items/executable_audio.sh
+++ dot_config/sketchybar/items/executable_audio.sh
@@ -2,9 +2,9 @@
 
 sketchybar --add item sound right         \
            --set sound update_freq=5      \
-                 sound script="$PLUGIN_DIR/sound.sh"
+                 script="$PLUGIN_DIR/sound.sh"
 
 sketchybar -m --add item mic right \
               --set mic update_freq=3 \
-                    mic script="$PLUGIN_DIR/mic.sh" \
-                    mic click_script="$PLUGIN_DIR/mic_click.sh"
+                    script="$PLUGIN_DIR/mic.sh" \
+                    click_script="$PLUGIN_DIR/mic_click.sh"
--- dot_config/sketchybar/items/executable_battery.sh
+++ dot_config/sketchybar/items/executable_battery.sh
@@ -3,5 +3,5 @@
 sketchybar -m --add item battery right \
               --set battery update_freq=120 \
                     updates=$HAS_BATTERY \
-                    battery script="$PLUGIN_DIR/battery.sh" \
-                    battery icon=
+                    script="$PLUGIN_DIR/battery.sh" \
+                    icon=
