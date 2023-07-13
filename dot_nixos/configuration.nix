0cf5972ea60a5a273ea720a87705a603596e8534
Michael <michaeljohannpeter@gmail.com>
2023-07-13 23:57:41 +0200

    nix: more binary patching

--- dot_nixos/configuration.nix
+++ dot_nixos/configuration.nix
@@ -3,6 +3,8 @@
 # Fix lua-language-server and stylua
 # patchelf --set-interpreter $(patchelf --print-interpreter `which find`) $HOME/.local/share/nvim/mason/packages/lua-language-server/libexec/bin/lua-language-server
 # patchelf --set-interpreter $(patchelf --print-interpreter `which find`) /home/michael/.local/share/nvim/mason/packages/stylua/stylua
+# libz.so.1 and glibc++6 not found in this case in this case
+# patchelf --set-rpath "$(nix eval nixpkgs#zlib.outPath --raw)/lib:$(nix eval nixpkgs#stdenv.cc.cc.lib.outPath --raw)/lib" .local/share/nvim/mason/bin/marksman
 
 
 { config, pkgs, ... }:
@@ -142,12 +144,14 @@
     acpi # battery interfae
     arandr # simple app to configure displays
     brightnessctl # for manipulating screen brightness
-    libnotify # notification library
     curl
     gcc
     git
     gnumake
     htop
+    icu.dev # dependecy for marksman
+    libnotify # notification library
+    libz # dependecy for marksman
     networkmanagerapplet # tray icon
     pulseaudio # for audio controlls
     udisks # query/manipulate storage devices
