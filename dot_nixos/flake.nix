8cc3a4bcfc4ecc310288260e73b76f87dcf3fcb1
Michael <michaeljohannpeter@gmail.com>
2023-09-16 20:16:57 +0200

    nix: switch to flakes

---
+++ dot_nixos/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake for thinkpad";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      thinkpad = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
