e4b7afee8c241e8e086a2813d61c5a808f6d53ad
Michael <michaeljohannpeter@gmail.com>
2024-01-09 23:08:48 +0100

    nix: Add home-manager support

--- dot_nixos/flake.nix
+++ dot_nixos/flake.nix
@@ -2,6 +2,8 @@
   description = "flake for thinkpad";
 
   inputs = {
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
@@ -15,12 +17,17 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       thinkpad = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           (import ./configuration.nix { inherit self; })
+          home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.users.michael = import ./home.nix;
+            }
         ];
       };
     };
