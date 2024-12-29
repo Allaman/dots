62646a10c1411feded2a37d03d656d721be4f598
Michael <michaeljohannpeter@gmail.com>
2024-12-29 14:01:06 +0100

    yazi: Use flavor instead of theme

---
+++ .chezmoiscripts/run_once_yazi-package-manager.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Running yazi package manager"
+ya pack -a BennyOe/tokyo-night
+ya pack -a yazi-rs/flavors:catppuccin-mocha
+ya pack -a yazi-rs/flavors:catppuccin-macchiato
+ya pack -a yazi-rs/flavors:catppuccin-frappe
