5e860922c0bc385fd65009aada7731d5cffaa96d
Michael <github@mjpeter.de>
2025-02-02 22:30:24 +0100

    bin: update search-bookmark script

--- dot_local/bin/executable_search-bookmark.sh
+++ dot_local/bin/executable_search-bookmark.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-bm --path "$HOME/data/bookmarks.sqlite" ls | fzf --with-nth=1 -d ';' | awk -F';' '{print $2}' | ccopy
+bm --path "$HOME/data/bookmarks.sqlite" ls -c | \
+  fzf --header="Bookmark Management" \
+  --header-first \
+  --ansi \
+  --margin="1%" \
+  --info="inline" \
+  --prompt="󰍉 " \
+  --pointer="󰁔" \
+  --with-nth=1 \
+  --color 'hl:cyan,hl+:cyan:bold' \
+  -d ';' | \
+  awk -F';' '{print $2}' \
+  | ccopy
