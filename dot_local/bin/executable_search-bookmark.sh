990208abd3d9709bd05db7c6226896f080b4fbc6
Michael <github@mjpeter.de>
2025-02-13 23:01:36 +0100

    bin: search-bookmarks.sh

--- dot_local/bin/executable_search-bookmark.sh
+++ dot_local/bin/executable_search-bookmark.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-bm --path "$HOME/data/bookmarks.sqlite" ls -c | \
+bm --path "$HOME/data/bookmarks.sqlite" ls -c |
   fzf --header="Bookmark Management" \
-  --header-first \
-  --ansi \
-  --margin="1%" \
-  --info="inline" \
-  --prompt="󰍉 " \
-  --pointer="󰁔" \
-  --with-nth=1 \
-  --color 'hl:cyan,hl+:cyan:bold' \
-  -d ';' | \
-  awk -F';' '{print $2}' \
-  | ccopy
+    --header-first \
+    --ansi \
+    --margin="1%" \
+    --info="inline" \
+    --prompt="󰍉 " \
+    --pointer="󰁔" \
+    --color 'hl:cyan,hl+:cyan:bold' \
+    -d '|' |
+  awk -F'|' '{print $2}' |
+  ccopy
