59ef7ae87c25b57ab478d0dbbfc56133e3c9a608
Michael <github@mjpeter.de>
2025-09-07 14:25:41 +0200

    yazi: update pugins

--- .chezmoiscripts/run_once_yazi-package-manager.sh
+++ .chezmoiscripts/run_once_yazi-package-manager.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Running yazi package manager"
-ya pack -a BennyOe/tokyo-night
-ya pack -a yazi-rs/flavors:catppuccin-mocha
-ya pack -a yazi-rs/flavors:catppuccin-macchiato
-ya pack -a yazi-rs/flavors:catppuccin-frappe
+ya pkg add BennyOe/tokyo-night
+ya pkg add yazi-rs/flavors:catppuccin-mocha
+ya pkg add yazi-rs/flavors:catppuccin-macchiato
+ya pkg add yazi-rs/flavors:catppuccin-frappe
+ya pkg add yazi-rs/plugins:git
