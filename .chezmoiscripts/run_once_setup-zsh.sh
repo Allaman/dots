0f4bf843bd8fae351ba343dbbb57d88ce32943d2
Michael <github@mjpeter.de>
2025-12-28 22:58:19 +0100

    chezmoi: Add run_once zsh plugins donwload script

---
+++ .chezmoiscripts/run_once_setup-zsh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.shell/zsh-autosuggestions
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.shell/fast-syntax-highlighting/
