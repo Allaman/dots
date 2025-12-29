b82fce255fa097d515f7faebefdf59c52003d793
Michael <github@mjpeter.de>
2025-12-29 19:04:18 +0100

    zsh: Fix run once zsh script

--- .chezmoiscripts/run_once_setup-zsh.sh
+++ .chezmoiscripts/run_once_setup-zsh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.shell/zsh-autosuggestions
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.shell/fast-syntax-highlighting/
+[ ! -d ~/.shell/zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.shell/zsh-autosuggestions || true
+[ ! -d ~/.fzf ] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install || true
+[ ! -d ~/.shell/fast-syntax-highlighting/ ] && git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.shell/fast-syntax-highlighting/ || true
