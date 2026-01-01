5276c0136603d2d39311588b7477901a2fac3d6c
Michael <github@mjpeter.de>
2026-01-01 17:57:29 +0100

    yazi: Fix deprecated API calls

--- dot_config/yazi/plugins/smart-enter.yazi/main.lua
+++ dot_config/yazi/plugins/smart-enter.yazi/main.lua
@@ -2,6 +2,6 @@
 return {
   entry = function(_, job)
     local h = cx.active.current.hovered
-    ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+    ya.mgr_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
   end,
 }
