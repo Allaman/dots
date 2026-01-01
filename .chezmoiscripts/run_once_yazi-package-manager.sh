66339c0c5152aa1aaff667de81679d2d76c527da
Michael <github@mjpeter.de>
2026-01-01 17:57:40 +0100

    yazi: Add cd to git root plugin

--- .chezmoiscripts/run_once_yazi-package-manager.sh
+++ .chezmoiscripts/run_once_yazi-package-manager.sh
@@ -6,3 +6,4 @@ ya pkg add yazi-rs/flavors:catppuccin-mocha
 ya pkg add yazi-rs/flavors:catppuccin-macchiato
 ya pkg add yazi-rs/flavors:catppuccin-frappe
 ya pkg add yazi-rs/plugins:git
+ya pkg add ciarandg/cd-git-root
