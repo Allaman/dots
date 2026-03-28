e3e50db1391a31b270d3840fbcc57e3ab64a16da
Michael <github@mjpeter.de>
2026-03-28 21:57:57 +0100

    zsh: Replace fast-syntax-highlighting with zsh-patina

--- .chezmoiscripts/run_once_setup-zsh.sh
+++ .chezmoiscripts/run_once_setup-zsh.sh
@@ -2,4 +2,3 @@
 
 [ ! -d ~/.shell/zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.shell/zsh-autosuggestions || true
 [ ! -d ~/.fzf ] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install || true
-[ ! -d ~/.shell/fast-syntax-highlighting/ ] && git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.shell/fast-syntax-highlighting/ || true
