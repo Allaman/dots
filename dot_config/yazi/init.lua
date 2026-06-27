b13ef3725909237a35778645478de576826e72b5
Michael <github@mjpeter.de>
2026-06-27 13:06:17 +0200

    yazi: Improve displayed infos

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -18,3 +18,17 @@ th.git.untracked_sign = "U"
 require("git"):setup({
   order = 1500,
 })
+
+function Linemode:size_and_mtime()
+  local time = math.floor(self._file.cha.mtime or 0)
+  if time == 0 then
+    time = ""
+  elseif os.date("%Y", time) == os.date("%Y") then
+    time = os.date("%b %d %H:%M", time)
+  else
+    time = os.date("%b %d  %Y", time)
+  end
+
+  local size = self._file:size()
+  return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
